65e83ad9bc163cb9a48df447f96d174966459103
askari01 <farrukh.askari01@gmail.com>
2019-03-31 14:44:36 +0500

    fixed typos

--- Sources/helloPackage/helloPackage.swift
+++ Sources/helloPackage/helloPackage.swift
@@ -1,16 +1,16 @@
 import Kitura
 import Foundation
 
-public class fireForDetector : RouterMiddleWare {
+public class fireForDetector : RouterMiddleware {
     public func handle(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
         if let ua = request.headers["User-Agent"], ua.contains ("Firefox") {
-            request.userInto["usingFirefox"] = true
+            request.userInfo["usingFirefox"] = true
         } else {
-            request.userInto["usingFirefox"] = false
+            request.userInfo["usingFirefox"] = false
         }
         next ()
     }
     public init() {
-        
+
     }
 }
